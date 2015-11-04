d6d2b1518ff22d6ca76b73007a304193ccb61b7e
astrapi69 <asterios.raptis@gmx.net>
2015-11-04 15:15:29 +0100

    Replaced strings with MediaType objects.

--- resource-bundles-cxf-api/src/main/java/de/alpharogroup/db/resource/bundles/rest/ResourcebundlesResource.java
+++ resource-bundles-cxf-api/src/main/java/de/alpharogroup/db/resource/bundles/rest/ResourcebundlesResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Component;
 
@@ -13,14 +14,13 @@ import de.alpharogroup.db.service.rs.AbstractRestfulResource;
 
 @Component
 @Path("/resourcebundle/")
-@Produces("application/json")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ResourcebundlesResource extends AbstractRestfulResource<Integer, Resourcebundle>
 {
     
     @GET
-    @Path("/get/{id}")
-    @Produces({"application/json"})
-    @Consumes({"application/json"})    
+    @Path("/get/{id}")   
     public Resourcebundle get(@PathParam("id")String id) {
     	Resourcebundle resourcebundle = getBusinessMapperService().read(Integer.valueOf(id));
     	return resourcebundle;
