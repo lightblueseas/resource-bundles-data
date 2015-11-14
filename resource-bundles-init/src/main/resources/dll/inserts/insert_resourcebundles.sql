07fa49e42f23586650aaf98a32acf5d87b27b1c9
astrapi69 <asterios.raptis@gmx.net>
2015-11-14 13:32:26 +0100

    upgraded from insert script with new entries

--- resource-bundles-init/src/main/resources/dll/inserts/insert_resourcebundles.sql
+++ resource-bundles-init/src/main/resources/dll/inserts/insert_resourcebundles.sql
@@ -1,2 +1,10 @@
-insert into resourcebundles (id, base_name, properties_key, locale, value) values(1,'base-resource-bundles','resource.bundles.test.label', 'de_DE', 'Erstes label');
-insert into resourcebundles (id, base_name, properties_key, locale, value) values(2,'base-resource-bundles','resource.bundles.test.label', 'UK', 'First label');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (1,'base-resource-bundles','resource.bundles.test.label', 'de_DE', 'Erstes label');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (2,'base-resource-bundles','resource.bundles.test.label', 'UK', 'First label');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (3,'test','com.example.gui.window.buttons.save','de_DE','Speichern');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (4,'test','com.example.gui.window.title','de_DE','Hallo, dort!');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (5,'test','com.example.gui.prop.with.params.label','de_DE','Hallo ich bin {0} und komme aus {1}.');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (6,'test','com.example.gui.window.buttons.cancel','de_DE','Abbrechen');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (7,'test','com.example.gui.window.buttons.save','en_GB','Save');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (8,'test','com.example.gui.window.title','en_GB','Hello, there!');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (9,'test','com.example.gui.prop.with.params.label','en_GB','Hello i am {0} and i come from {1}.');
+INSERT INTO resourcebundles (id, base_name, properties_key, locale, value) VALUES (10,'test','com.example.gui.window.buttons.cancel','en_GB','Cancel');
