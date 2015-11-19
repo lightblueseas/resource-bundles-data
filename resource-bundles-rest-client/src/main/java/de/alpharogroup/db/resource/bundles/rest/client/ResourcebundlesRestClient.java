0b1c074a8873f5c8e971ed00ff568cd5cb807439
astrapi69 <asterios.raptis@gmx.net>
2015-11-19 16:39:24 +0100

    Added default base url.

--- resource-bundles-rest-client/src/main/java/de/alpharogroup/db/resource/bundles/rest/client/ResourcebundlesRestClient.java
+++ resource-bundles-rest-client/src/main/java/de/alpharogroup/db/resource/bundles/rest/client/ResourcebundlesRestClient.java
@@ -14,32 +14,46 @@ import de.alpharogroup.db.resource.bundles.rest.api.ResourcebundlesResource;
 import lombok.Getter;
 
 /**
- * The class {@link ResourcebundlesRestClient} is a rest client for the resource-bundles that are persists in the database.
+ * The class {@link ResourcebundlesRestClient} is a rest client for the
+ * resource-bundles that are persists in the database.
  */
-public class ResourcebundlesRestClient
-{
+public class ResourcebundlesRestClient {
+
+	/** The Constant DEFAULT_HTTP_PORT. */
+	public static final int DEFAULT_HTTP_PORT = 8080;
+
+	/** The Constant BASE_URL_PREFIX. */
+	public static final String BASE_URL_PREFIX = "http://localhost";
+
+	/** The Constant DEFAULT_BASE_URL. */
+	public static final String DEFAULT_BASE_URL = BASE_URL_PREFIX + ":" + DEFAULT_HTTP_PORT;
 	
 	/**
-	 * Gets the resourcebundles resource.
-	 *
-	 * @return the resourcebundles resource
+	 * The {@link ResourcebundlesResource}.
 	 */
 	@Getter
 	private final ResourcebundlesResource resourcebundlesResource;
 
 	/**
+	 * Instantiates a new {@link ResourcebundlesRestClient} with the default base url.
+	 */
+	public ResourcebundlesRestClient() {
+		this(DEFAULT_BASE_URL);
+	}
+
+	/**
 	 * Instantiates a new {@link ResourcebundlesRestClient}.
 	 *
-	 * @param baseUrl the base url
+	 * @param baseUrl
+	 *            the base url
 	 */
-	public ResourcebundlesRestClient(String baseUrl)
-	{
-        List<Object> providers = new ArrayList<>();
-        providers.add(new JacksonJsonProvider());
-        
-        resourcebundlesResource  = JAXRSClientFactory.create(baseUrl, ResourcebundlesResource.class, providers);
-        WebClient.client(resourcebundlesResource).accept(MediaType.APPLICATION_JSON);
-        WebClient.client(resourcebundlesResource).type(MediaType.APPLICATION_JSON);
+	public ResourcebundlesRestClient(String baseUrl) {
+		List<Object> providers = new ArrayList<>();
+		providers.add(new JacksonJsonProvider());
+
+		resourcebundlesResource = JAXRSClientFactory.create(baseUrl, ResourcebundlesResource.class, providers);
+		WebClient.client(resourcebundlesResource).accept(MediaType.APPLICATION_JSON);
+		WebClient.client(resourcebundlesResource).type(MediaType.APPLICATION_JSON);
 	}
 
 }
