61e2aee6493572521cbf35ee12defd14a22a6d5d
astrapi69 <asterios.raptis@gmx.net>
2015-12-10 17:28:17 +0100

    parent pom version update for extend from abstract rest client

--- resource-bundles-rest-client/src/main/java/de/alpharogroup/db/resource/bundles/rest/client/ResourcebundlesRestClient.java
+++ resource-bundles-rest-client/src/main/java/de/alpharogroup/db/resource/bundles/rest/client/ResourcebundlesRestClient.java
@@ -1,15 +1,6 @@
 package de.alpharogroup.db.resource.bundles.rest.client;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.ws.rs.core.MediaType;
-
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
-import org.apache.cxf.jaxrs.client.WebClient;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-
+import de.alpharogroup.cxf.rest.client.AbstractRestClient;
 import de.alpharogroup.db.resource.bundles.rest.api.ResourcebundlesResource;
 import lombok.Getter;
 
@@ -17,17 +8,8 @@ import lombok.Getter;
  * The class {@link ResourcebundlesRestClient} is a rest client for the
  * resource-bundles that are persists in the database.
  */
-public class ResourcebundlesRestClient {
+public class ResourcebundlesRestClient extends AbstractRestClient {
 
-	/** The Constant DEFAULT_HTTP_PORT. */
-	public static final int DEFAULT_HTTP_PORT = 8080;
-
-	/** The Constant BASE_URL_PREFIX. */
-	public static final String BASE_URL_PREFIX = "http://localhost";
-
-	/** The Constant DEFAULT_BASE_URL. */
-	public static final String DEFAULT_BASE_URL = BASE_URL_PREFIX + ":" + DEFAULT_HTTP_PORT;
-	
 	/**
 	 * The {@link ResourcebundlesResource}.
 	 */
@@ -47,13 +29,9 @@ public class ResourcebundlesRestClient {
 	 * @param baseUrl
 	 *            the base url
 	 */
-	public ResourcebundlesRestClient(String baseUrl) {
-		List<Object> providers = new ArrayList<>();
-		providers.add(new JacksonJsonProvider());
-
-		resourcebundlesResource = JAXRSClientFactory.create(baseUrl, ResourcebundlesResource.class, providers);
-		WebClient.client(resourcebundlesResource).accept(MediaType.APPLICATION_JSON);
-		WebClient.client(resourcebundlesResource).type(MediaType.APPLICATION_JSON);
+	public ResourcebundlesRestClient(final String baseUrl) {
+		super(baseUrl);
+		resourcebundlesResource = newResource(ResourcebundlesResource.class);
 	}
 
 }
