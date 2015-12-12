a6231f5572f8d94106ac2126cb54f10e67a73c70
astrapi69 <asterios.raptis@gmx.net>
2015-12-12 14:13:52 +0100

    Mapper updated after refactoring.

--- resource-bundles-domain/src/main/java/de/alpharogroup/db/resource/bundles/mapper/ResourcebundlesMapper.java
+++ resource-bundles-domain/src/main/java/de/alpharogroup/db/resource/bundles/mapper/ResourcebundlesMapper.java
@@ -2,7 +2,7 @@ package de.alpharogroup.db.resource.bundles.mapper;
 
 import org.springframework.stereotype.Component;
 
-import de.alpharogroup.db.entitymapper.AbstractEntityBOMapper;
+import de.alpharogroup.db.entitymapper.AbstractEntityDOMapper;
 import de.alpharogroup.db.resource.bundles.domain.Resourcebundle;
 import de.alpharogroup.db.resource.bundles.entities.Resourcebundles;
 
@@ -10,5 +10,5 @@ import de.alpharogroup.db.resource.bundles.entities.Resourcebundles;
  * The class {@link ResourcebundlesMapper}.
  */
 @Component
-public class ResourcebundlesMapper extends AbstractEntityBOMapper<Resourcebundles, Resourcebundle> {
+public class ResourcebundlesMapper extends AbstractEntityDOMapper<Resourcebundles, Resourcebundle> {
 }
