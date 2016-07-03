478e83e5cf18c6e4985fda0f6bedf329d9c213ab
Asterios Raptis <asterios.raptis@gmx.net>
2016-07-03 14:51:54 +0200

    Bugfix after update from insert script

--- resource-bundles-rest-web/src/main/resources/dll/inserts/insert_resourcebundles.sql
+++ resource-bundles-rest-web/src/main/resources/dll/inserts/insert_resourcebundles.sql
@@ -24,11 +24,13 @@ INSERT INTO bundlenames (id, version, base_name_id, locale_id) VALUES (1, 1, 1, 
 INSERT INTO bundlenames (id, version, base_name_id, locale_id) VALUES (2, 1, 1, 2);
 INSERT INTO bundlenames (id, version, base_name_id, locale_id) VALUES (3, 1, 2, 1);
 INSERT INTO bundlenames (id, version, base_name_id, locale_id) VALUES (4, 1, 2, 3);
+INSERT INTO bundlenames (id, version, base_name_id, locale_id) VALUES (5, 1, 1, 7);
 
 INSERT INTO default_locale_basenames (id, version, bundlename_id, default_locale_id) VALUES (1, 1, 1, 1);
 INSERT INTO default_locale_basenames (id, version, bundlename_id, default_locale_id) VALUES (2, 1, 2, 1);
 INSERT INTO default_locale_basenames (id, version, bundlename_id, default_locale_id) VALUES (3, 1, 3, 3);
 INSERT INTO default_locale_basenames (id, version, bundlename_id, default_locale_id) VALUES (4, 1, 4, 3);
+INSERT INTO default_locale_basenames (id, version, bundlename_id, default_locale_id) VALUES (5, 1, 5, 4);
 
 INSERT INTO properties_keys (id, version, name) VALUES (1, 1, 'resource.bundles.test.label');
 INSERT INTO properties_keys (id, version, name) VALUES (2, 1, 'com.example.gui.window.buttons.save');
@@ -46,3 +48,4 @@ INSERT INTO resourcebundles (id, version, properties_key_id, value, bundlename_i
 INSERT INTO resourcebundles (id, version, properties_key_id, value, bundlename_id) VALUES (8, 1, 3, 'Hello, there!', 4);
 INSERT INTO resourcebundles (id, version, properties_key_id, value, bundlename_id) VALUES (9, 1, 4, 'Hello i am {0} and i come from {1}.', 4);
 INSERT INTO resourcebundles (id, version, properties_key_id, value, bundlename_id) VALUES (10, 1, 5, 'Cancel', 4);
+INSERT INTO resourcebundles (id, version, properties_key_id, value, bundlename_id) VALUES (11, 1, 1, 'Erstes label', 5);
