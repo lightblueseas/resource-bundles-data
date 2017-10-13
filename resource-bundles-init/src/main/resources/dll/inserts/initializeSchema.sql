ea6fa6f9b8d7388309c98822f87d73df9f80173c
Asterios Raptis <asterios.raptis@gmx.net>
2017-10-13 14:02:13 +0200

    update scripts to new db-schema

--- resource-bundles-init/src/main/resources/dll/inserts/initializeSchema.sql
+++ resource-bundles-init/src/main/resources/dll/inserts/initializeSchema.sql
@@ -5,12 +5,6 @@
         name varchar(512),
         primary key (id)
     );
-create table bundle_application_bundlenames (
-        application_id int4 not null,
-        bundlenames_id int4 not null unique,
-        primary key (application_id, bundlenames_id),
-        unique (bundlenames_id)
-    );
 create table bundle_application_language_locales (
         application_id int4 not null,
         language_locales_id int4 not null,
@@ -29,6 +23,7 @@ create table bundlenames (
         filepath varchar(4096),
         base_name_id int4,
         locale_id int4,
+        owner_id int4,
         primary key (id)
     );
 create table language_locales (
@@ -60,12 +55,11 @@ create table resourcebundles (
     );
 
 
-alter table bundle_application_bundlenames add constraint FKE03DE2DA81FDD8D7 foreign key (bundlenames_id) references bundlenames;
-alter table bundle_application_bundlenames add constraint FKE03DE2DA64F45D92 foreign key (application_id) references bundle_applications;
 alter table bundle_application_language_locales add constraint FKC6BB891EFB766A66 foreign key (language_locales_id) references language_locales;
 alter table bundle_application_language_locales add constraint FKC6BB891E64F45D92 foreign key (application_id) references bundle_applications;
 alter table bundle_applications add constraint FK176B41C017DEE600 foreign key (default_locale_id) references language_locales;
 alter table bundlenames add constraint FKF230A806D4CC327E foreign key (locale_id) references language_locales;
+alter table bundlenames add constraint FKF230A80614CE514F foreign key (owner_id) references bundle_applications;
 alter table bundlenames add constraint FKF230A80663C76715 foreign key (base_name_id) references basenames;
 alter table resourcebundles add constraint FKD0A7106365054731 foreign key (properties_key_id) references properties_keys;
 alter table resourcebundles add constraint FKD0A71063BE71D570 foreign key (bundlename_id) references bundlenames;
