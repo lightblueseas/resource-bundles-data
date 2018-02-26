28fc7be5cac360b57747d64ed8e1b7fd8334c720
Asterios Raptis <asterios.raptis@gmx.net>
2018-02-26 13:33:38 +0100

    Update of dll scripts

--- resource-bundles-rest-web/src/main/resources/dll/dropSchema.sql
+++ resource-bundles-rest-web/src/main/resources/dll/dropSchema.sql
@@ -1,14 +1,12 @@
-alter table bundle_application_bundlenames drop constraint FKE03DE2DA81FDD8D7;
-alter table bundle_application_bundlenames drop constraint FKE03DE2DA64F45D92;
 alter table bundle_application_language_locales drop constraint FKC6BB891EFB766A66;
 alter table bundle_application_language_locales drop constraint FKC6BB891E64F45D92;
 alter table bundle_applications drop constraint FK176B41C017DEE600;
 alter table bundlenames drop constraint FKF230A806D4CC327E;
+alter table bundlenames drop constraint FKF230A80614CE514F;
 alter table bundlenames drop constraint FKF230A80663C76715;
 alter table resourcebundles drop constraint FKD0A7106365054731;
 alter table resourcebundles drop constraint FKD0A71063BE71D570;
 drop table basenames;
-drop table bundle_application_bundlenames;
 drop table bundle_application_language_locales;
 drop table bundle_applications;
 drop table bundlenames;
